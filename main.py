c77df6705b0005a291faa6edac8371c2ff3e7099
Nitesh Sen <n.sen@prioticket.com>
2024-11-22 13:49:35 +0530

    add Day 10 file

---
+++ main.py
@@ -0,0 +1,19 @@
+a = input()
+print(a)
+a = input("Enter Your Name: ")
+print(a)
+b = input("Enter any number: ")
+print("Your Lucky number is", b)
+u = 1
+w = 2
+
+u = float(input("Enter 1st Number: ")) # this number you can write decmal value also
+w = int(input("Enter Second Number: ")) # in this you can write decimal values
+print(u - w)
+
+g = input("Enter 1st Number: ") # this number you can write decmal value also
+e = input("Enter Second Number: ") # in this you can write decimal values
+# print(u + w) # this is not working with any symble (+ - * ?)
+print(int(g) / int(e))
+print(int(g) - int(e))
+
