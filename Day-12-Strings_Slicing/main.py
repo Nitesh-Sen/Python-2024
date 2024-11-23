54e27e4e3bb659b9fc0f126d8f92c559eea2ea6a
Nitesh Sen <n.sen@prioticket.com>
2024-11-23 11:51:08 +0530

    Add file Day-12-Strings_Slicing

---
+++ Day-12-Strings_Slicing/main.py
@@ -0,0 +1,28 @@
+name = "Neon,Rummy"
+print(name[0:])
+print(name[0:3])
+print(name[0:2],name[5])
+print(name[0:2] + name[5])
+print(name[5:8])
+# print(name[0:4,5:]) # Not working
+print(name[0:4] + name[5:])
+print(name[0:4], name[5:])
+
+print("\nnexT Type:")
+fruit = "Mango"
+magoLen = len(fruit)
+print(magoLen)
+print(fruit[:3] + ";)")
+print(fruit, "is delecious")
+print(fruit[0:-3]) # -----------\
+print(fruit[:len(fruit)-2]) # <-     # -------------------\
+print(fruit[0:len(fruit)-1]) # This is equal like above. you can add 0 or make it empty. Python will automatically start it.
+print(fruit[-7:len(fruit)-0]) # This program will not throw the error is just give you all words and other give as empty.
+print(fruit + "s")
+
+
+# Quick Quiz (Comment your result without run code)
+print("\n# Quick Quiz")
+nm = "Neone"
+print(nm[-4:-2])
+# print(nm[-5:-2])
