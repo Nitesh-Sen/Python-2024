641a0eb98781e1f235f576b6c1ec03f85cf06146
Nitesh Sen <n.sen@prioticket.com>
2024-11-23 11:00:06 +0530

    Add files of Day-11-Strings

---
+++ Day-11-Strings/main.py
@@ -0,0 +1,31 @@
+name = "Neon"
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+# print(name[4]) # Throws an error
+print("\nLets use a for loop:")
+for c in name: 
+    print(c)
+print()
+
+Address = "India"
+print(Address)
+print()
+
+apple = 'He said, \nHi Neon \nHow are you?'
+print(apple)
+print()
+
+repple = '''He said,
+Hi Neon 
+Hey I am good.
+"I want to see movie"'''
+print(repple)
+print()
+
+test = "Hi my name is Neon and I am from \"India\""
+print(test)
+print()
+
