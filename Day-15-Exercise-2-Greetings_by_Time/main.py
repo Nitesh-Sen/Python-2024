575c6f25b8b20d9aab5a4593f7200c49e7be2416
Nitesh Sen <n.sen@prioticket.com>
2024-11-27 17:25:08 +0530

    Upload Day-15-Exersize

---
+++ Day-15-Exercise-2-Greetings_by_Time/main.py
@@ -0,0 +1,12 @@
+import time
+a = int(time.strftime("%H"))
+name = "Nitesh Sen"
+if(0 > a < 12):
+    print("Good Morning", name)
+elif(a < 17):
+    print("Good Afternoon", name)
+elif(a < 21):
+    print("Good Evening", name)
+else:
+    print("Good Night", name)
+
