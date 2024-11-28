3f22a6ad892d3e487a47d68ff4d477ec2cddc942
Nitesh-Sen <niteshsen515@gmail.com>
2024-11-28 15:54:30 +0530

    Add files Day-18-While_Loops

---
+++ Day-18-While_Loops/main.py
@@ -0,0 +1,21 @@
+i = 0
+while(i<=3):
+    print(i)
+    i = i +1
+
+print("Done with the loop")
+
+b=12
+while(i<=b):
+    i = int(input("Enter any number: "))
+    print(i)
+
+print("This loop is complete. Because", i, "is greater than", b)
+
+# Decreament loop
+count = 5
+while (count > -2):
+    print(count)
+    count = count - 1
+else:
+    print("you have reached else part")
