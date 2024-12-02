625ba14a869cd4bd86d970c689ebfaaf4d76f67e
Nitesh-Sen <n.sen@prioticket.com>
2024-12-02 13:54:40 +0530

    Add files of Day-22

---
+++ Day-22-Introduction_to_Lists/main.py
@@ -0,0 +1,14 @@
+marks = [3, 5, 7]
+print(marks)
+print(type(marks))
+print(marks[1])
+print(marks[2])
+
+marks = [3, 5, 7, "Delhi", "is capital"] # This list Index is 4, But lenth is 5.
+print(marks)
+print(type(marks))
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
+print(marks[0:4:2])
