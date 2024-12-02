144ce2808467ecae5cb5a352710c592da421db0b
Nitesh-Sen <n.sen@prioticket.com>
2024-12-02 12:32:32 +0530

    Add files of Day-21

--- Day-21-Function_arguments/main.py
+++ Day-21-Function_arguments/main.py
@@ -12,3 +12,11 @@ average(b=3)
 def averagE(a, b, c=1):
     print("The average is", (a+b+c)/2)
 averagE(5,6)
+
+def average(*number):
+    print(type(number))
+    sum = 0
+    for i in number:
+        sum = sum + i
+    print("Average is:", sum/len(number))
+average(5, 6, 7)
