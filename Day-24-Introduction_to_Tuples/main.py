9c7e032d8cad36b7a073a1fba6785aa6f173c7bf
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-03 13:57:54 +0530

    Add Day24 files

--- Day-24-Introduction_to_Tuples/main.py
+++ Day-24-Introduction_to_Tuples/main.py
@@ -2,5 +2,6 @@ tup = (1, 2, 76, 342, "Yellow", False)
 # tup[0] = 90 # Tupple values is not changeable. So, if you need to change any number. Then you should use List type class.
 print(type(tup), tup)
 
+
 if 2 in tup:
     print("Yes \"2\" is present in this tuple")
