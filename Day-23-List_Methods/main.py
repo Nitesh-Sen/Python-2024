20b324dff209ce979a111300b0b3f6ec23c15a31
Nitesh-Sen <n.sen@prioticket.com>
2024-12-03 13:46:25 +0530

    Add file of Day-23-Functions

---
+++ Day-23-List_Methods/main.py
@@ -0,0 +1,10 @@
+list = [11, 32, 54, 65, 43]
+print(list)
+list.append(87)
+print(list.sort(reverse=True))
+list.reverse
+print(list.index(32))
+print(list.count(65))
+r = list
+r[0] =7
+print(list)
