79368cd936d5a2a493bdabd0a5b12d3bbc8fe4fb
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-04 17:42:40 +0530

    Add files of Day-29-Docstrings

---
+++ Day-29-Docstrings/main.py
@@ -0,0 +1,14 @@
+def square(n):
+    '''Takes in a number n, returns the cube of n'''
+    print(n**3)
+square(3)
+print(square.__doc__)
+print()
+
+#another example
+def cube(n):
+    print(n)
+    '''Takes in a number n, returns the cube of n'''
+    print(n**3)
+cube(3)
+print(cube.__doc__)
