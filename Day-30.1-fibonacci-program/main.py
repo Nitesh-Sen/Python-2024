7f091edefa4b12139f65512ca64a546ec1cb3b85
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-05 12:08:12 +0530

    Add file Day-30.1-fibonacci-program/main.py

--- Day-30.1-fibonacci-program/main.py
+++ Day-30.1-fibonacci-program/main.py
@@ -15,3 +15,17 @@ def fib(n):
         # b = a
         # a = b + a
 fib(10)
+print()
+
+# 2nd program
+def fibb(n):
+    a = 0
+    b = 1
+    print(a, end=" ")
+    print(b, end=" ")
+    for i in range(n):
+        c = a + b
+        a = b
+        b = c
+        print(c, end=" ")
+fibb(7)
