c170b961779b23ebbecc86ea973acd20c8cec842
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-06 18:31:58 +0530

    Add

--- Day-36-Exception_Handling/main.py
+++ Day-36-Exception_Handling/main.py
@@ -8,8 +8,34 @@ if a.isdigit():
     for i in range(11):
         print(f"{int(a)} X {i} = {int(a)*i}")
 
-# else a.isdecimal():
+# else a.isdecimal(): # This can be also happen
 else:
     print("you are in else part============>")
     for i in range(11):
-        print(f"{float(a)} X {i} = {float(a)*i}")
+        print(f"{float(a)} X {i} = {float(a)*i:.2f}")
+        # print(f'{float(a)} X {i} = {float(a)*(i)}')
+
+
+# Another Program::======================>>>>
+# We can use this program when we don't want to stop our program with error in one command or any case. 
+# If trriger any error. Than we can just print the error or any output and continue other program without any interption.
+
+b = input("Enter the Another number: ")
+print(f"Multiplication table of Antoher number {b} is: ")
+try: 
+    if a.isdigit():
+        print("you are in if part of another program===========>")
+        for i in range(11):
+            print(f"{int(b)} X {i} = {int(b)*i}")
+
+    # else a.isdecimal():
+    else:
+        print("you are in else part of another program============>")
+        for i in range(11):
+            print(f"{float(b)} X {i} = {float(b)*i:.2f}")
+# except Exception as e:
+#     print(e)
+except:
+    print(f"Invaild input {b}!!!!")
+
+print("\nSome line of another code.\nEnd of program")
