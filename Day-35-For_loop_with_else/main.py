a773182588414bd9d87e719e0fe862898981f850
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-06 17:30:46 +0530

    Add

---
+++ Day-35-For_loop_with_else/main.py
@@ -0,0 +1,16 @@
+for i in range(6):
+    print(i)
+    # if i == 4: 
+    #     break
+else:
+    print("Sorry no i") # Else part is not run if loop will be break. That's why I commented that break part. Else part will be run after complete the for loop part, but not run after break.
+
+# Second Method
+i = 8
+while i < 12:
+    print(i)
+    i = i +1
+    # if i == 11:
+    #     break
+else:
+    print("Sorry no 2nd i")
