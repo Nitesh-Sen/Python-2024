1f0e408d2135ff10eafa329ac7c2dd514d13ae74
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-06 10:57:57 +0530

    Add file Day-31-Sets

---
+++ Day-31-Sets/main.py
@@ -0,0 +1,14 @@
+set = {3, 9, 9, 12} # Sets are unchangeable. Set do not contain duplicate items.
+print(set)
+
+info = {"Kerla", 90, False, 3.7, 42, 3.7}
+print(info)
+
+ni = {} # we can create empty set with {}. we have to use set()
+print(type(ni))
+
+# mii = set()     # Not Working
+# print(type(mii))
+
+for ord in info:
+    print(ord)
