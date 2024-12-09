3eda3a2ab34aa66c98e58cda26b092ee787f48ba
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-09 11:25:13 +0530

    Add

--- Day-36-Exception_Handling/main.py
+++ Day-36-Exception_Handling/main.py
@@ -1,17 +1,16 @@
 a = input("Enter the number of Table: ")
 print(f"Multiplication table of {a} is: ")
-i = 1
 # b = type(a).__name__
 # print(b)
 if a.isdigit():
     print("you are in if part===========>")
-    for i in range(11):
+    for i in range(1, 11):
         print(f"{int(a)} X {i} = {int(a)*i}")
 
 # else a.isdecimal(): # This can be also happen
 else:
     print("you are in else part============>")
-    for i in range(11):
+    for i in range(1, 11):
         print(f"{float(a)} X {i} = {float(a)*i:.2f}")
         # print(f'{float(a)} X {i} = {float(a)*(i)}')
 
@@ -23,21 +22,40 @@ else:
 
 b = input("Enter the Another number: ")
 print(f"Multiplication table of Antoher number {b} is: ")
-try: 
-    if a.isdigit():
-        print("you are in if part of another program===========>")
-        for i in range(11):
-            print(f"{int(b)} X {i} = {int(b)*i}")
 
-    # else a.isdecimal():
-    else:
-        print("you are in else part of another program============>")
-        for i in range(11):
-            print(f"{float(b)} X {i} = {float(b)*i:.2f}")
-# except Exception as e:
-#     print(e)
-except:
-    print(f"Invaild input {b}!!!!")
+if b.isdigit():
+    print("you are in if part of another program===========>")
+    for i in range(1, 11):
+        print(f"{int(b)} X {i} = {int(b)*i}")
+# else a.isdecimal():
+else:
+    print("you are in else part of another program============>")
+    for i in range(1, 11):
+        # print(f"{float(b)} X {i} = {float(b)*i:.2f}")
+        ans = round(float(b) * float(i), 4)
+        # print(f"{ans:.4f}")
+        print(f"{b} X {i} = {ans:.4f}")
+
+#========================================================
+# b = input("Enter the Another number: ")
+# print(f"Multiplication table of Antoher number {b} is: ")
+# try: 
+#     if b.isdigit():
+#         print("you are in if part of another program===========>")
+#         for i in range(11):
+#             print(f"{int(b)} X {i} = {int(b)*i}")
+
+#     # else a.isdecimal():
+#     else:
+#         print("you are in else part of another program============>")
+#         for i in range(11):
+#             # print(f"{float(b)} X {i} = {float(b)*i:.2f}")
+#             print(float(b), "X", i, round((b * i), 2))
+# # except Exception as e:
+# #     print(e)
+# except:
+#     print(f"Invaild input {b}!!!!")
+#====================================================
 #########################################################
 # We can also use this for handling the specific error.
 # and I have commented these below lines because these both work at same time. but not working with above "except" condtion.
