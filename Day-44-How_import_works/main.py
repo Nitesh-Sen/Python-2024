445022a3da171e631ed85fe42cfb0c8a8f69ebfd
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-11 11:15:57 +0530

    Add

---
+++ Day-44-How_import_works/main.py
@@ -0,0 +1,35 @@
+import pandas
+
+# import math
+# math.floor(4.2332)
+
+# from math import sqrt, pi
+# result = sqrt(9) * pi
+# print(result)
+
+# from math import *
+# result = sqrt(16) * pi
+# print(result)
+
+# from math import sqrt as s, pi
+# result = s(16) * pi
+# print(result)
+
+# import math as m
+# result = m.sqrt(81) * m.pi
+# print(result)
+
+# import math as math_builtin_python
+# result = math_builtin_python.sqrt(63) * math_builtin_python.pi
+# print(result)
+
+# import math
+# print(dir(math))
+# print(math.log2)
+# print(math.nan)
+# print(math.pi)
+# print(math.sin)
+
+from nit import neon, welcome
+print(welcome)
+print(neon)
---
+++ Day-44-How_import_works/nit.py
@@ -0,0 +1,4 @@
+def welcome():
+    # print("Hey you are welcome in my code")
+    print("Hey you are welcome my friend")
+neon = "A Bad Boy"
