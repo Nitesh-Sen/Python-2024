e770e991691acb6cb7c51ffe08d02f6a4e2bf796
Nitesh-Sen <niteshsen515@gmail.com>
2024-12-15 21:56:29 +0530

    Add

---
+++ Day-55-Exerise-5-SnakeWaterGun/Example.py
@@ -0,0 +1,6 @@
+# Snake, Gun and Water
+#                 S W G
+# computer =      1 2 3
+# player   = S 1  D W L 
+#            W 2  L D W 
+#            G 3  W L D 
---
+++ Day-55-Exerise-5-SnakeWaterGun/main.py
@@ -0,0 +1,39 @@
+import random
+youPoint = botPoint = i = 0
+num = 1
+print(youPoint, botPoint, i)
+while i < 10:
+    print(f"\n===============> Game {num} <===============")
+    You = input("1. Snake\n2. Water\n3. Gun\nChoose a number in [1-3]: ")
+    if not You.strip():
+        print("Value is empty. Try Again...!")
+    elif You.isdigit():
+        You = int(You)
+        if 0 < You < 4:
+            i = i + 1
+            num = num + 1
+            print(f"You Choose: {You}")
+            Bot = random.randint(1,3)
+            print(f"Bot Choose: {Bot}")
+            if You is Bot:
+                print("Draw.")
+            elif You == 1 and Bot == 2 or You == 2 and Bot == 3 or You == 3 and Bot == 1:
+                print("You Win..!")
+                youPoint = youPoint + 1
+            elif You == 1 and Bot == 3 or You == 2 and Bot == 1 or You == 3 and Bot == 2:
+                print("You Loose..!")
+                botPoint = botPoint + 1
+        else:
+            print("Invalid Input...!")
+    else:
+        print("Invalid input. Please enter valid Number.")
+
+
+print("\n")
+print("Final is ==============>")
+if botPoint == youPoint:
+    print(f"Game is Draw..............!\nYour Point is: {youPoint}\nBot Point is: {botPoint}")
+elif botPoint > youPoint:
+    print(f"You loose the Game............!\nYour Point is: {youPoint}\nBot Point is: {botPoint}")
+else:
+    print(f"You win the Game............!\nYour Point is: {youPoint}\nBot Point is: {botPoint}")
